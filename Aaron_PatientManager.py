45bcc52aac276723ebb216b2eb9e50e5faed1643
Aaron Ly <aaron.ly@edu.sait.ca>
2023-04-24 20:56:17 -0600

    Updated documention for PatientManager Class

--- Aaron_PatientManager.py
+++ Aaron_PatientManager.py
@@ -1,13 +1,20 @@
+"""
+This is the patient manager class for the Project Classes assignment. In this file will include methods for the user to use to create, display, and edit the patients' data.
+This class also has a constructor to create a list to fill the patients' data with. Also, this class will import the patient class to be able to create and update patient objects.
+
+Group: Aaron, Mae, Sang, Alex
+Due Date: April 26, 2023
+"""
 import patient
 
 
 class PatientManager:
 
-    def __init__(self):
+    def __init__(self):  # Constructor, creates a empty list for patients and fill it with the read_patients_file method.
         self.__patientsLists = []  # Empty list for patients
         PatientManager.read_patients_file(self)
 
-    def format_patient_info_for_file(self, new_patient):
+    def format_patient_info_for_file(self, new_patient):  # This function used to format the patient data.
         return patient.Patient.__str__(new_patient)
 
     def enter_patient_info(self):  # This function ask user to enter the new patients information and creates a new patient with it
@@ -42,49 +49,48 @@ class PatientManager:
             elif counter == len(self.__patientsLists):  # This is a condition use to display a message for the user, once the for loop has gone through all the patients on the list.
                 print("Can't find the Patient with the same id on the system")
 
-    def display_patient_info(self, person):
-        if person in self.__patientsLists:
+    def display_patient_info(self, person):  # This function takes a patient object and displays the patient information.
+        if person in self.__patientsLists:  # Checks if patient is in list
             print(person)
 
-    def edit_patient_info_by_id(self):
+    def edit_patient_info_by_id(self):  # This function will prompt the user for an ID number and let the user change information for that patient.
         user_input = input("Please enter the id of the Patient that you want to edit their information: ")
-        counter = 0
+        counter = 0  # Counter use to keep track of how many iterations the for loop has gone.
         for person in self.__patientsLists:
             counter += 1
-            patient_id = str(person).split("_")[0]
-            if user_input == patient_id:
+            patient_id = str(person).split("_")[0]  # Use to get only the ID for the patient data.
+            if user_input == patient_id:  # Check if ID matches.
                 new_name = str(input("Enter new name: "))
                 new_disease = str(input("Enter new disease: "))
                 new_gender = str(input("Enter new gender: "))
                 new_age = str(input("Enter new age:"))
-                updated_patient = patient.Patient(patient_id, new_name, new_disease, new_gender, new_age)
-                self.__patientsLists[self.__patientsLists.index(person)] = updated_patient
-                PatientManager().write_list_of_patients_to_file(self.__patientsLists)
-                break
+                updated_patient = patient.Patient(patient_id, new_name, new_disease, new_gender, new_age)  # Creates the new patient object.
+                self.__patientsLists[self.__patientsLists.index(person)] = updated_patient  # Replace old patient object with the updated one.
+                PatientManager().write_list_of_patients_to_file(self.__patientsLists)  # Uses write_list_of_patients_to_file method to save new list.
+                break  # End for loop once user has finished updating.
             elif counter == len(self.__patientsLists):  # This is a condition use to display a message for the user, once the for loop has gone through all the patients on the list.
                 print("Can't find the Patient with the same id on the system")
 
         print("\nPatient whose ID is", patient_id, "has been edited.")
 
-    def display_patients_list(self):
+    def display_patients_list(self):  # This function will go through each patient in the list and display the information with the correct format.
         print(f'\n{"ID":<5s}{"Name":<24s}{"Disease":<18s}{"Gender":<18s}{"Age"}\n')  # Formats and displays the information template.
 
         for person in self.__patientsLists:
             ID, name, disease, gender, age = [str(data) for data in str(person).split("_")]  # Assigns each patient properties to its corresponding variable.
             print(f'{ID:<5s}{name:<24s}{disease:<18s}{gender:<18s}{age}\n')  # Formats and displays the patient's information.
 
-    def write_list_of_patients_to_file(self, patients_list):
+    def write_list_of_patients_to_file(self, patients_list):  # This function will write the current patient list to the patient.txt file.
         patient_file = open('patients.txt', 'w')
         self.__patientsLists = patients_list
         for x in patients_list:
             patient_file.write(str(x) + "\n")
         patient_file.close()
 
-    def add_patient_to_file(self):
-        new_patient = PatientManager().enter_patient_info()
-        self.__patientsLists.append(new_patient)
-        data = PatientManager().format_patient_info_for_file(new_patient)
-        print(data)
+    def add_patient_to_file(self):  # This function will add a new patient to the patient.txt file.
+        new_patient = PatientManager().enter_patient_info()  # Uses the enter_patient_info method to get the new patients' information.
+        self.__patientsLists.append(new_patient)  # Adds the new patient to the list.
+        data = PatientManager().format_patient_info_for_file(new_patient)  # Formats the data
         patient_id = str(data).split("_")[0]
         patient_file = open('patients.txt', 'a+')
         patient_file.write("\n" + data)
